fe5de95665a670fc8011f7655941a88d553a8e25
Mclow27 <Moechokr27@gmail.com>
2022-06-23 13:39:25 -0400

    Factory Design Pattern
    Factory Design Pattern

--- Login-pattern/original.js
+++
@@ -1,7 +0,0 @@
-import userLogger from './UserLogged.js'
-
-export default function firstLog() {
-    userLogger.printLogCount()
-    userLogger.log('File')
-    userLogger.printLogCount()
-}
--- Login-pattern/pattern.js
+++ Login-pattern/pattern.js
@@ -1,22 +1,54 @@
-class UserLogged {
-    constructor() {
-      if (UserLogged.instance == null) {
-        this.logs = []
-        UserLogged.instance = this
-      }
-      return UserLogged.instance
+var foodOption = function () {
+    this.createuser = function (choice) {
+        var user;
+
+        if (choice === "Creamy Garlic Shrimp") {
+            user = new Food1();
+        } else if (choice === "Pesto Penne Pasta") {
+            choice = new Food2();
+        } else if (choice === "Glazed Honey Balsamic Pork Chops") {
+            user = new Food3();
+        } else if (choice === "Creamy Tuscan Chicken") {
+            user = new Food4();
+        }
+
+        user.type = type;
+
+        user.say = function () {
+            console.log(this.choice + ": Food option " + this.food + " ");
+        }
+
+        return user;
     }
-  
-    log(message) {
-      this.logs.push(message)
-      console.log(`LOG: ${message}`)
+}
+
+var Food1 = function () {
+    this.food = "Creamy Garlic Shrimp";
+};
+
+var Food2 = function () {
+    this.food = "Pesto Penne Pasta";
+};
+
+var Food3 = function () {
+    this.food = "Glazed Honey Balsamic Pork Chops";
+};
+
+var Food4 = function () {
+    this.food = "Creamy Tuscan Chicken";
+};
+
+function run() {
+
+    var users = [];
+    var factory = new foodOption();
+
+    users.push(factory.createuser("Creamy Garlic Shrimp"));
+    users.push(factory.createuser("Pesto Penne Pasta"));
+    users.push(factory.createuser("Glazed Honey Balsamic Pork Chops"));
+    users.push(factory.createuser("Creamy Tuscan Chicken"));
+
+    for (var i = 0, len = users.length; i < len; i++) {
+        users[i].say();
     }
-  
-    printLogCount() {
-      console.log(`${this.logs.length} Logs`)
-    }
-  }
-  
-  const userLogger = new UserLogged()
-  Object.freeze(userLogger)
-  export default userLogger
+}
