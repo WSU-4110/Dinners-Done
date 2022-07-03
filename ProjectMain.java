2bd41ae64f1a855cde02a1b1e681234609054e7e
ctshea <49501347+ctshea@users.noreply.github.com>
2022-07-03 14:16:31 -0400

    Created Recipe class and everything needed for recipe objects
    Created a recipe class we can choose to implement/edit if we keep, as well as all the logic needed to create efficient recipe classes that shows their own name, serving size, web address, and ingredients, as well as how to output each of those. the Recipe class I was using is at the bottom below the commented code to see the tables.

--- ProjectMain.java
+++ ProjectMain.java
@@ -3,54 +3,73 @@ import java.sql.*;
 public class ProjectMain {
     public static void main(String[] args) {
 
+        Recipe[] recipe = new Recipe[20];
+        double[] ingredients = new double[101]; //main array used for calculations
 
-        String url = "jdbc:sqlite:CSC4100Project/src/main/resources/RecipesDB.db"; //path from content root
+        String[][] ingredientsUnitName = new String[101][2]; //used so we can easily output without another query
+        //first array being ingredient name, second being units for that ingredient.
+        //get ID and amount of ingredient for each recipe object
+
+        String url = "jdbc:sqlite:src/main/resources/RecipesDB.db"; //path from content root
         try {
 
-
-            //QUERY FOR WHEN USER FAVORITES RECIPE
-            //INSERT INTO user_recipe(idRecipes, idUser)
-            //VALUES(RECIPE ID THAT THEY CLICKED, USER ID);
-
-            //QUERY FOR WHEN USER UNFAVORITES RECIPE
-            //delete from user_recipe
-            //where idRecipes = RECIPE ID THAT THEY CLICKED and idUser = USER ID;
-
-            //QUERY TO SHOW USER THEIR FAVORITES
-            //select R.name from recipes R inner join user_recipe U2R on
-            //R.idRecipes = U2R.idRecipes where U2R.idUser = USER ID;
-
-            //QUERY TO INSERT NEW RECIPE INTO TABLE
-            //INSERT INTO recipes(name, amountFed, link,userAdded,userIdThatAdded)
-            //VALUES("MEAL NAME", AMOUNT FED, "LINK", 'y', USER ID);
-
-            //QUERY TO DELETE RECIPE THEY CREATED FROM TABLE
-            //delete from recipes
-            //where name = "MEAL NAME";
-
-            //QUERY TO SHOW USER THE RECIPES THAT THEY CREATED
-            //select name from recipes where userIdThatAdded = 2;
-
-
             // query that shows records needed from each recipe, this example uses recipe 3 as seen in R2I.idrecipe = 3"
-            String query = "select I.name, I.unit, amount from ingredients I inner join recipe_ingredient R2I" +
-                    " on I.idingredients = R2I.idingredient where R2I.idrecipe = 3";
+            //String query = "select I.name, I.unit, amount from ingredients I inner join recipe_ingredient R2I" +
+            // " on I.idingredients = R2I.idingredient where R2I.idrecipe = 3";
+
+            //ResultSet rset = statement.executeQuery(query);
 
             Connection connection = DriverManager.getConnection(url);
             Statement statement = connection.createStatement();
 
-            ResultSet rset = statement.executeQuery(query);
+            String nameQuery = "select name, amountFed, link from recipes"; //query for name, serving size, and web address
+            ResultSet rset = statement.executeQuery(nameQuery);
+            int counter = 0;
+            while(rset.next()) {   // process each row
+                recipe[counter] = new Recipe(rset.getString("name"), rset.getInt("amountFed"), rset.getString("link"));
+                counter++;
+            }
 
+            counter = 0;
+            String ingredientsQuery = "select name, unit from ingredients"; //query for ingredient name and unit for outputting ingredients for each object
+            rset = statement.executeQuery(ingredientsQuery);
+            while (rset.next()) {
+                ingredientsUnitName[counter][0] = rset.getString("name");
+                ingredientsUnitName[counter][1] = rset.getString("unit");
+                counter++;
+            }
 
-/*            while(rset.next()) {   // process each row
+            //set size of individual recipe object arrays
+            for (int i = 1; i <= recipe.length; i++) {
+                rset = statement.executeQuery("select count(idingredient) from recipe_ingredient where idrecipe = " + i);
+                recipe[i-1].setArraySize(rset.getInt("count(idingredient)"));
+                recipe[i-1].setIngredientsID(new int[recipe[i-1].getArraySize()]);       //declare size of their arrays
+                recipe[i-1].setIngredientsAmount(new double[recipe[i-1].getArraySize()]); //declare size of their arrays
+            }
+
+            //intialize data in the recipe object arrays
+            for (int i = 1; i <= recipe.length; i++) {
+                counter = 0;
+                rset = statement.executeQuery("select idingredient, amount from recipe_ingredient where idrecipe = " + i);
+                while (rset.next()){
+                    recipe[i - 1].ingredientsID[counter] = rset.getInt("idingredient");
+                    recipe[i - 1].ingredientAmount[counter] = rset.getDouble("amount");
+                    counter++;
+                }
+            }
+
+            //how to output the information from each object, this shows object in index 1 as example. just change recipe[index] for different object.
+            System.out.println(recipe[1].getName() + "\t\t" + recipe[1].getAmountFed() + "\t\t" + recipe[1].getWebAddress());
+            for (int i = 0; i < recipe[1].getArraySize(); i++) {
+                System.out.println(recipe[1].ingredientAmount[i] + "\t" + ingredientsUnitName[i][1] + "\t" + ingredientsUnitName[i][0]);
+            }
+
+            /*while(rset.next()) {   // process each row
                 String name = rset.getString("name");      // get name of ingredient
                 double amount = rset.getDouble("amount");  // get amount of ingredient used in recipe
                 String unit = rset.getString("unit");      // get unit of measurement for ingredient
                 System.out.printf("%10.2f%10s%30s%n", amount, unit, name);
             }*/
-
-
-
 
 /*            /////////////////////////////////////////////////////
             // functionality ideas for user searching database //
@@ -73,12 +92,12 @@ public class ProjectMain {
 
 
 
-            ////////////////////////////////////
+/*            ////////////////////////////////////
             //To see all data in recipes table//
             ////////////////////////////////////
             String queryRecipeData = "select * from recipes";
             System.out.println("\nData from recipes table");
-            System.out.printf("%5s%20s%20s%50s%%15s%10sn", "ID", "NAME", "serving size" , " link", "userAdded", "userThatAdded");
+            System.out.printf("%5s%20s%20s%50s%n", "ID", "NAME", "serving size" , " link");
             rset = statement.executeQuery(queryRecipeData);
 
             while(rset.next()) {   // process each row
@@ -86,9 +105,7 @@ public class ProjectMain {
                 String recipeName = rset.getString("name");
                 int amountFed = rset.getInt("amountFed");
                 String urlLink = rset.getString("link");
-                String userAdded = rset.getString("userAdded");
-                int userIdThatAdded = rset.getInt("userIdThatAdded");
-                System.out.printf("%5d%35s%5d%80s%5s%10d%n",idRecipe, recipeName, amountFed, urlLink, userAdded, userIdThatAdded);
+                System.out.printf("%5d%30s%5d%80s%n",idRecipe, recipeName, amountFed, urlLink);
             }
 
             ////////////////////////////////////
@@ -121,26 +138,73 @@ public class ProjectMain {
                 double amount = rset.getDouble("amount");
 
                 System.out.printf("%5d%10d%10d%20.2f%n", idRecipeIngredient, idRecipes, idIngredients, amount);
-            }
-
-            //////////////////////////////////////////////
-            //To see all data in user_recipe table//        -- there is none until users add
-            //////////////////////////////////////////////
-            String queryUserJoinedTableData = "select * from user_recipe";
-            System.out.println("\nData from user_recipe table");
-            System.out.printf("%5s%12s%15s%n", "ID", "recipeID", "userId");
-            rset = statement.executeQuery(queryUserJoinedTableData);
-
-            while(rset.next()) {   // process each row
-                int idUserRecipe = rset.getInt("idUserRecipes");
-                int idRecipes = rset.getInt("idRecipes");
-                int idUser = rset.getInt("idUser");
-
-                System.out.printf("%5d%10d%15d%n", idUserRecipe, idRecipes, idUser);
-            }
+            }*/
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+}
+class Recipe {
+
+    int[] ingredientsID;
+    double[] ingredientAmount;
+    int arraySize;
+    String name;
+    String webAddress;
+    int amountFed;
+
+    public Recipe(String name, int amountFed, String link) {
+        this.name = name;
+        this.webAddress = link;
+        this.amountFed = amountFed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWebAddress(String webAddress) {
+        this.webAddress = webAddress;
+    }
+
+    public void setAmountFed(int amountFed) {
+        this.amountFed = amountFed;
+    }
+
+    public void setIngredientsID(int[] ingredientsID) {
+        this.ingredientsID = ingredientsID;
+    }
+
+    public void setArraySize(int arraySize) {
+        this.arraySize = arraySize;
+    }
+
+    public void setIngredientsAmount(double[] ingredientsAmount) {
+        this.ingredientAmount = ingredientsAmount;
+    }
+
+    public double[] getIngredientsAmount() {
+        return ingredientAmount;
+    }
+
+    public int[] getIngredientsID() {
+        return ingredientsID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWebAddress() {
+        return webAddress;
+    }
+
+    public int getAmountFed() {
+        return amountFed;
+    }
+
+    public int getArraySize() {
+        return arraySize;
     }
 }
