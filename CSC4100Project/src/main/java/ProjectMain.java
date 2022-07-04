99cf663862be69b245b781fcec31e132daa44950
ctshea <49501347+ctshea@users.noreply.github.com>
2022-07-04 19:14:12 -0400

    Updated ProjectMain to use builder design pattern
    Updated ProjectMain to allow the user to create a recipe with a builder design pattern implementation.

--- CSC4100Project/src/main/java/ProjectMain.java
+++ CSC4100Project/src/main/java/ProjectMain.java
@@ -1,48 +1,19 @@
 import java.sql.*;
+import java.util.Scanner;
+
 
 public class ProjectMain {
     public static void main(String[] args) {
-
-
+        MakeRecipe MR = new MakeRecipe();
+        Recipe recipe = MR.buildRecipe(new OurDatabaseRecipeBuilder());
         String url = "jdbc:sqlite:src/main/resources/RecipesDB.db"; //path from content root
         try {
-
             // query that shows records needed from each recipe, this example uses recipe 3 as seen in R2I.idrecipe = 3"
             String query = "select I.name, I.unit, amount from ingredients I inner join recipe_ingredient R2I" +
                     " on I.idingredients = R2I.idingredient where R2I.idrecipe = 3";
-
             Connection connection = DriverManager.getConnection(url);
             Statement statement = connection.createStatement();
-
             ResultSet rset = statement.executeQuery(query);
-
-            while(rset.next()) {   // process each row
-                String name = rset.getString("name");      // get name of ingredient
-                double amount = rset.getDouble("amount");  // get amount of ingredient used in recipe
-                String unit = rset.getString("unit");      // get unit of measurement for ingredient
-                System.out.printf("%10.2f%10s%30s%n", amount, unit, name);
-            }
-
-/*            /////////////////////////////////////////////////////
-            // functionality ideas for user searching database //
-            /////////////////////////////////////////////////////
-            //every time event on search bar happens
-            //clear database results that may be shown
-            //loop though array of ingredients
-            //check if userinput.containsIgnoreCase(ingredients[i])
-            //if it does use i+1 in query search -> database starts at 1
-            //this example uses chicken which would is idingredient 14 as shown in ingredient table
-            String searchRecipes = "select R.name from recipes R inner join recipe_ingredient R2I" +
-                    " on R.idRecipes = R2I.idrecipe where R2I.idingredient = 14";
-
-            rset = statement.executeQuery(searchRecipes);
-
-            while(rset.next()) {   // process each row
-                String name = rset.getString("name");      // get name of recipe
-                System.out.println("\n" + name + "\n");
-            }*/
-
-
 
 /*            ////////////////////////////////////
             //To see all data in recipes table//
@@ -95,5 +66,141 @@ public class ProjectMain {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+}
+
+class Recipe {
+    String name;
+    int amountFed;
+    final String userAdded = "y";
+    int[] unitOfMeasurement;
+    double amountOfIngredient;
+    int[] ingredients;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmountFed(int amountFed) {
+        this.amountFed = amountFed;
+    }
+
+    public void setIngredient(int[] ingredient) {
+        this.ingredients = ingredient;
+    }
+
+    public void setAmountOfIngredient(double amountOfIngredient) {
+        this.amountOfIngredient = amountOfIngredient;
+    }
+
+}
+
+abstract class RecipeBuilder {
+    protected Recipe recipe = new Recipe();
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public abstract void buildName();
+    public abstract void buildAmountFed();
+    public abstract void buildIngredient();
+    public abstract void buildAmountOfIngredient();
+}
+
+class OurDatabaseRecipeBuilder extends RecipeBuilder { //for user to build a recipe with our ingredients
+    Scanner scanner = new Scanner(System.in);
+    @Override
+    public void buildName() {
+        String userInputName = scanner.nextLine();	//user input for name
+        recipe.setName(userInputName);
+    }
+
+    @Override
+    public void buildAmountFed() {
+        int userInputAmountFed = scanner.nextInt();	//user input for serving size
+        recipe.setAmountFed(userInputAmountFed); //the int will be user input for how many people the meal feeds
+    }
+
+
+    @Override
+    public void buildIngredient() {
+        int userInputNumIngredients = scanner.nextInt();	//get number of ingredients user will be adding
+        int ingredients[] = new int[userInputNumIngredients];
+        for (int i = 0; i < ingredients.length; i++) {
+            ingredients[i] = scanner.nextInt();				//get the ingredient id from user
+        }
+        recipe.setIngredient(ingredients);
+    }
+
+
+    @Override
+    public void buildAmountOfIngredient() {
+        double userInputAmountIngredient = scanner.nextDouble();	//get number of ingredients user will be adding
+        recipe.setAmountOfIngredient(userInputAmountIngredient); //the int will be how much the user wants to add of that ingredient
+    }
+}
+
+class NewIngredientRecipeBuiler extends RecipeBuilder { //for user to build a recipe with ingredients they add
+    Scanner scanner = new Scanner(System.in);
+    @Override
+    public void buildName() {
+        String userInputName = scanner.nextLine();	//user input for name
+        recipe.setName(userInputName);
+    }
+
+    @Override
+    public void buildAmountFed() {
+        int userInputAmountFed = scanner.nextInt();	//user input for serving size
+        recipe.setAmountFed(userInputAmountFed); //the int will be user input for how many people the meal feeds
+    }
+
+
+    @Override
+    public void buildIngredient() {
+        PreparedStatement prep;
+        int userInputArraySize = scanner.nextInt();	//user input for how many ingredients he will add
+        int[] ingredients = new int[userInputArraySize];
+        String ingredientName;
+        String unitOfMeasurement;
+        Connection connection = null;
+        ResultSet rset;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/RecipesDB.db");
+            Statement statement = connection.createStatement();
+            ingredientName = scanner.next();
+            unitOfMeasurement = scanner.next();
+            for (int i = 0; i < userInputArraySize; i++) {
+                prep = connection.prepareStatement("insert into ingredients values (" +ingredientName + ", " + unitOfMeasurement + ")", Statement.RETURN_GENERATED_KEYS);
+                rset = prep.getGeneratedKeys();
+                ingredients[i] = rset.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        recipe.setIngredient(ingredients);
+    }
+
+
+    @Override
+    public void buildAmountOfIngredient() {
+        double userInputAmountIngredient = 0;
+        int userInputSize = scanner.nextInt();	//user input for how many ingredients did you add
+        for (int i = 0; i < userInputSize; i++) {
+            userInputAmountIngredient = scanner.nextDouble();	//get number of ingredients user will be adding
+        }
+        recipe.setAmountOfIngredient(userInputAmountIngredient); //the int will be how much the user wants to add of that ingredient
+    }
+}
+
+class MakeRecipe {
+
+    public Recipe buildRecipe(RecipeBuilder builder) {
+        builder.buildName();
+        builder.buildAmountFed();
+        builder.buildIngredient();
+        builder.buildAmountOfIngredient();
+        return builder.getRecipe();
     }
 }
