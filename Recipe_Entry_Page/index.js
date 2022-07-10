8a3e7025609ac16b76b0a97885f28e027a4371d4
Bruno Martins <gz4256@wayne.edu>
2022-07-09 21:18:09 -0400

    removed old folder

--- Recipe_Entry_Page/index.js
+++
@@ -1,8 +0,0 @@
-function SaveRecipe() {
-    var RecipeName = document.getElementById("RecipeName").value;
-    var RecipeAuthor = document.getElementById("RecipeAuthor").value;
-    var RecipeUrl = document.getElementById("RecipeUrl").value;
-    var Recipe = document.getElementById("Recipe").value;
-
-    alert("Recipe Name: " + RecipeName + "\nRecipe Author: " + RecipeAuthor + "\nRecipe Url: " + RecipeUrl + "\nRecipe Instructions: \n" + Recipe);
-}
