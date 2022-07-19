7643b49aefb7a170f6c9cb9ac31fc6fc384cfc3b
ogudino <98849725+ogudino2@users.noreply.github.com>
2022-07-18 20:57:24 -0400

    added new code

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="">
+    <section>
+      <div class="box">
+        <div class="rbox" v-for="recipe in recipes" v-bind:key="recipe.id">
+          <a :href="recipe.link">
+            <img class="recipe-img" :src="recipe.image" >
+            <br>
+            <div class="recipe-name">{{recipe.name}}</div>
+            <br>
+          </a>
+
+          <div class="recipe-name">Author: {{recipe.author}}</div>
+            <br>
+            <div class="recipe-name">Amount Fed: {{recipe.amount_fed}}</div>
+            <br>
+            <div class="recipe-name">
+              <b>Ingredients:</b>
+              <ul v-for="ingredient in recipe.ingredients" v-bind:key="ingredient.name + ingredient.size">
+                <li style="display: block; color: black"> Name: {{ingredient.name }}</li>
+                <li style="display: block; color: black">Amount: {{ingredient.amount }}</li>
+                <li style="display: block; color: black">Size: {{ingredient.size }} <br></li>
+                <div style="height: 10px; display: block"></div>
+              </ul>
+            </div>
+          <div>
+            <br><button type="button" class="favbtn" v-if="user.loggedIn">Add Favorite</button>
+            <br><button type="button" class="favbtn">Add to Shopping List</button>
+            <select name="Quantity" class="foodquantity">
+              <option value="o1">1</option>
+              <option value="o2">2</option>
+              <option value="o3">3</option>
+              <option value="o4">4</option>
+              <option value="o5">5</option>
+              <option value="o6">6</option>
+              <option value="o7">7</option>
+              <option value="o8">8</option>
+              <option value="o9">9</option>
+              <option value="o10">10</option>
+            </select><br>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+
+import {db} from "@/main";
+import {mapGetters} from "vuex";
+// import {collection } from "firebase/firestore"
+
+export default {
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+  created() {
+    //this.$route.params.id  .doc()
+    //  db.collection("recipes").get().then((data) =>
+    //  this.recipes = data.docs );
+    db.collection('recipes')
+        .get()
+        .then(querySnapshot => {
+          console.log(querySnapshot);
+          this.recipes = querySnapshot.docs.map(doc => doc.data())
+          // do something with documents
+        })
+
+  },
+  methods: {
+    submit() {
+    }
+  },
+  computed: {
+    // map `this.user` to `this.$store.getters.user`
+    ...mapGetters({
+      user: "user"
+    })
+  }
+
+
+};
+</script>
+
+<style>
+@import '../assets/style.css';
+</style>
+
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="container">
+    <nav class="menu">
+      <input type="checkbox" id="toggle-bar">
+      <label for="toggle-bar" class='btn'>
+        <i class="fa fa-bars"></i>
+      </label>
+    </nav>
+    <section>
+      <div class="container">
+        <div class="title"><span>Register</span></div>
+        <form action="#" @submit.prevent="submit">
+          <div class="text">
+
+            <input type="text" placeholder="Enter your name" v-model="form.name" required>
+          </div>
+          <div class="text">
+
+            <input type="text" placeholder="Enter Email" v-model="form.email" required>
+          </div>
+
+          <div class="text">
+            <input type="password" v-model="form.password" placeholder="Enter Password" id="password" required>
+          </div>
+          <div class="text">
+            <input type="password" v-model="form.password_confirmation"  placeholder="Confirm password" id="password2" required >
+          </div>
+          <div class="buttn">
+            <input type="submit" value="Signup">
+            <li v-for="error in form.error" style="color:gray; text-align: left;  padding-left: 40px;" v-bind:key="error">{{error}}</li>
+
+          </div>
+          <div class="login">Already have an account? <a href="login.html"> login Now </a></div>
+
+        </form>
+
+      </div>
+
+
+
+    </section>
+  </div>
+</template>
+
+
+<script>
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: "",
+        error: [],
+      },
+      error: null
+    };
+  },
+  methods: {
+    validateForm(){
+      var valid = true;
+      this.form.error =[];
+
+
+      if(this.form.name.length === 0 ){
+        this.form.error.push("The username field is required") ;
+
+        valid = false;
+
+      }
+      if(this.form.email.length === 0 ){
+        this.form.error.push("The email field is required") ;
+        valid = false;
+      }
+
+      if(this.form.password !== this.form.password_confirmation){
+        this.form.error.push("The passwords do not match") ;
+        valid = false;
+
+      }
+      if(this.form.password.length < 6 || this.form.password.length > 14 ){
+        this.form.error.push("The password length should be between 6 and 14") ;
+
+        valid = false;
+
+      }
+      return valid
+    },
+
+    submit() {
+      if(this.validateForm()) {
+
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
+          .then((user) => {
+            if (user) {
+              updateProfile(auth.currentUser, {
+                displayName: this.form.name,
+                photoURL: ''
+              })
+            }
+            this.$router.push('/');
+
+          })
+          .catch(err => {
+            this.error = err.message;
+          });
+      }
+    }
+  }
+};
+</script>
+
+<style>
+/*@import '../assets/style.css';*/
+</style>
+
