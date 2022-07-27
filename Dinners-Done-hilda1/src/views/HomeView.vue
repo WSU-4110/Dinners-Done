068e08ad529ec1e301a05203310fe66353b82561
McLow27 <95956527+McLow27@users.noreply.github.com>
2022-07-27 17:26:33 -0400

    Add files via upload

---
+++ Dinners-Done-hilda1/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <section>
+        <div class="content">
+            <h1>Welcome To Dinners Done</h1>
+            <p> Here we make it easier for you to prepare food. Simply all you need to do is look for a recipe,<br> print it and then begin shopping. </p>
+       
+            <a href="/dashboard" class="button"> Get Started</a>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+}
+</script>
