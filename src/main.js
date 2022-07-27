8258c603ebff7ea6c3cb97beea0da7a66c5bf5c0
Hq9390 <105396819+Hq9390@users.noreply.github.com>
2022-07-27 14:32:30 -0400

    cleaned up main.js and added comments

--- src/main.js
+++ src/main.js
@@ -2,18 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import * as firebase from "firebase/app";
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-
-// import { getDatabase } from "firebase/database";
-
-// import { initializeApp } from "firebase/app";
-//
+//Firebase configuration 
 const firebaseConfig = {
     apiKey: "AIzaSyBGU_GbRz664n7KY8xU8GsT1_DfJMx0oqA",
     authDomain: "recipe-dd.firebaseapp.com",
@@ -24,22 +17,17 @@ const firebaseConfig = {
     measurementId: "G-P18ZGNZN2D"
 };
 
+// initialize firebase application
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+//create a globally accessible firestore object
 export const db = firebaseApp.firestore();
 firebase.auth().onAuthStateChanged(user => {
     store.dispatch("fetchUser", user);
 });
 
 
-
-//
-// export const db = getDatabase(firebaseApp);
-// export const auth = getAuth(firebaseApp);
-
-// export const db = firebase.firestore();
-// var db = firebase.firestore()
-
+// initialize VueJs and mount it on the div with id of app
 createApp(App).use(store).use(router).mount('#app')
 
 
